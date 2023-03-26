258fc984207582af3b9e2346c1e7d487cf11a4f4
Aysylu Fahretdinova(laptop) <icelookkool@yandex.ru>
2023-03-27 00:18:53 +0300

    add solution for task 0, 1, 3, 5, 7

---
+++ Seminar1task03/Program.cs
@@ -0,0 +1,14 @@
+﻿string? numline = Console.ReadLine();
+if (numline != null)
+{
+   int num = int.Parse(numline);
+   int startnum = num * -1;
+   string result = string.Empty;
+   while (startnum <= num)
+   {
+    result = result + startnum + " ";
+    startnum = startnum + 1;
+    // startnum ++
+   }
+   Console.WriteLine(result);
+}
