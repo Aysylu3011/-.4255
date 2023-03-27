bdb53754bd31f4b383c2c0b687960d9b8fc44eb4
Aysylu Fahretdinova(laptop) <icelookkool@yandex.ru>
2023-03-27 15:46:18 +0300

    HW1_Task1

---
+++ HomeWorkTask01/Program.cs
@@ -0,0 +1,28 @@
+﻿// 1
+int numberA = 5, 
+    numberB = 7;
+int max = numberA;
+if (numberA > numberB) max = numberA;
+if (numberA < numberB) max = numberB;
+Console.WriteLine(max);
+
+// 2
+int numberC = 2, 
+    numberD = 10;
+int maximum = numberC;
+if (numberC > numberD) maximum = numberC;
+if (numberC < numberD) maximum = numberD;
+Console.WriteLine(maximum);
+
+// 3
+int numberE = -9; 
+int numberF = -3;
+int maxi = numberE;
+if (numberE !=  null && numberF != null)
+{
+    //int numE = int.Parse(numberE);
+    //int numF = int.Parse(numberF);
+    if (((numberE * -1) + numberF) > 0) maxi = numberF;
+    if (((numberE * -1) + numberF) < 0) maxi = numberE;
+    Console.WriteLine(maxi);
+}
