d039276e41bebd2afdc785537464419d26c80e3a
Aysylu Fahretdinova(laptop) <icelookkool@yandex.ru>
2023-04-06 00:08:10 +0300

    Задание 23

---
+++ HW3Task23/Program.cs
@@ -0,0 +1,23 @@
+﻿// Подается число, нужно вывести таблицу кубов от 1 до введенного числа
+
+int ReadData(string msg)
+{
+    Console.WriteLine(msg);
+    int res = int.Parse(Console.ReadLine() ?? "0");
+
+    return res;
+}
+
+string LineGenerator(int num, int pow)
+{
+    string res = string.Empty;
+    for (int i = 1; i <= num; i++)
+    {
+        res += Math.Pow(i, pow) + " ";
+    }
+    return res;
+}
+
+int number = ReadData("ВВедите число: ");
+Console.WriteLine(LineGenerator(number, 1));
+Console.WriteLine(LineGenerator(number, 3));
