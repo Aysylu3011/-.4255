7bc35739c9422f3352dfb386a26fa88744ba0601
Aysylu Fahretdinova(laptop) <icelookkool@yandex.ru>
2023-04-08 15:02:24 +0300

    Задание 27

---
+++ HW4task27/Program.cs
@@ -0,0 +1,25 @@
+﻿int ReadData(string msg)
+{
+    Console.WriteLine(msg);
+    int res = int.Parse(Console.ReadLine() ?? "0");
+
+    return res;
+}
+
+int sumOfNum(int num)
+{
+    int summa = 0;
+    char [] digits = num.ToString().ToCharArray();
+    int count = digits.Length;
+    while(count > 0)
+    {
+        summa = summa + (num % 10);
+        num = num / 10;
+        count = count -1;
+    }
+    return summa;
+
+}
+
+int numberA = ReadData("Введите число: ");
+Console.WriteLine("Сумма всех цифр числа: " + sumOfNum(numberA));
